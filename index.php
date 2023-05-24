f8bccc04c44c8800426254f94169a67a8a088be9
khanwardak <n.khan.css47@gmail.com>
2023-05-24 21:41:26 +0430

    some css applyed into index page

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
- 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SSO</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="login">
+        <div class="card">
+          <div class="card-title">
+            <h3>Welcome to SSO App</h3>
+          </div>
+          <div class="card-body d-flex justify-content-center">
+            <div class="d-flex justify-content-center">
+              <a href="#" class="btn btn-dark" style="margin-top:10px">Login With
+                <i class="fa fa-github fa-3x" style="margin-top:10px"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html
